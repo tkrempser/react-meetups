39fa7fdc064767891243c653d48f9cdd84ad09e7
Thiago Krempser <tkrempser@gmail.com>
2021-08-25 19:49:08 -0300

    Bug fix for class name attribute

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -2,9 +2,9 @@ import styles from "./Spinner.module.css";
 
 function Spinner() {
   return (
-    <div class={styles.spinner}>
-      <div class={styles["bounce-1"]}></div>
-      <div class={styles["bounce-2"]}></div>
+    <div className={styles.spinner}>
+      <div className={styles["bounce-1"]}></div>
+      <div className={styles["bounce-2"]}></div>
       <div></div>
     </div>
   );
