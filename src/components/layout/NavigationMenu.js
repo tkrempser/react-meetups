8ec1524a703042e74f61519ed1fca7f4500ad2a7
Thiago Krempser <tkrempser@gmail.com>
2021-08-26 17:44:05 -0300

    Add favorites badge

--- src/components/layout/NavigationMenu.js
+++ src/components/layout/NavigationMenu.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./NavigationMenu.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function NavigationMenu() {
+  const favoriteContext = useContext(FavoritesContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function NavigationMenu() {
             <Link to="/react-meetups/new/">New Meetup</Link>
           </li>
           <li>
-            <Link to="/react-meetups/favorites/">Favorites</Link>
+            <Link to="/react-meetups/favorites/">
+              Favorites
+              <span className={styles.badge}>
+                {favoriteContext.favorites.length}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
